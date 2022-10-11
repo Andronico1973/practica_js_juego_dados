51cb20dd678a93a63d2a7f890c519bb345cb67ad
Patricio <patricio.arenasmanterola@everis.nttdata.com>
2022-10-11 17:21:36 -0300

    actualizacion script.js

--- script.js
+++ script.js
@@ -13,17 +13,28 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
 
 // Condiciones iniciales
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
 
-const scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+const init = function(){
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    playing = true;
 
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    player0El.classList.remove('player--winner');
+    player1El.classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+init();
 
 const swichPlayer = function() {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -65,7 +76,7 @@ btnHold.addEventListener('click', function(){
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
     // 2 verificar si el score de los jugadores es >= 100
-   if (scores[activePlayer] >= 20) {
+   if (scores[activePlayer] >= 100) {
     // terminar el juego 
     playing = false;
     document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
@@ -77,4 +88,6 @@ btnHold.addEventListener('click', function(){
 }
 
     
-})
+});
+
+btnNew.addEventListener('click', init());
